176332086dd84f087df02f77c832edad2b445df4
EnmAsusW <enmacastle@outlook.es>
2022-11-18 20:33:15 -0300

    Experimentacion

--- pythonbase/RELPandMatematics.py
+++ pythonbase/RELPandMatematics.py
@@ -1,7 +1,13 @@
+
+#En  un gran resumen podemos decir que las formas basicas de hacer matematicas (Aritmetica) es la misma que en C++
+
+
+
 #if 8 > 5: #Una pequema introducción a las condiciones
     #print("La wea Cosmica che")
     
-    #Vamos a trabajar unas listas
+    
+    #VAMOS A TRABAJAR LAS LISTAS
     
 lista = ['xd','Bola','Jala',]
 lista2=lista.copy() #Nos permite copiar
@@ -26,7 +32,11 @@ lista.append('Hola') #Metodo append nos ayuda a agregar valores a nuestra lista.
 lista.reverse() #Esto nos permite volcar todo alreves (Reverso)  #NOTA IMPORTANTE!!!
 # NO PODEMOS ORDENAR DATOS DE DIFERENTES TIPOS DEBEN SER SIEMPRE DEL MISMO TIPO CUANDO USAMOS O METODO DE sort
 print(lista)
+tuplas = ('itos1', 'cabeza de mojon', 'me vale vergz') #Tupla não muda seus dados como a lista
 
-lista.sort()
-print(lista)
+lista.sort() #Es una forma de ordenar listas ya sea en orden alfabetico o numerico pero solo puede recibir un tipo de dato
+print(lista) 
+print(tuplas)
+
+
 
