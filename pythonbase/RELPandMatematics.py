0c8f16757e8609c3ff0982504e8243d47835983b
EnmAsusW <enmacastle@outlook.es>
2022-11-18 20:16:25 -0300

    Modificação add list e apagado de arquivos não necessarios

---
+++ pythonbase/RELPandMatematics.py
@@ -0,0 +1,32 @@
+#if 8 > 5: #Una pequema introducción a las condiciones
+    #print("La wea Cosmica che")
+    
+    #Vamos a trabajar unas listas
+    
+lista = ['xd','Bola','Jala',]
+lista2=lista.copy() #Nos permite copiar
+lista.append('Hola') #Metodo append nos ayuda a agregar valores a nuestra lista.
+ 
+#print(lista, lista2)
+
+#Contar elementos y ver cuantas veces se repite dentro de nuestras listas
+
+#print(lista, lista2.count(3))
+
+#print(len(lista), len(lista2)) #La funcion len nos mostrara cuantos elementos existen dentro de nuestra lista
+
+# Vaciar nuestra lista ---> list.clear()
+
+#print(lista[0]) #Aqui podemos imprimir algun elemento de nuestra lista... Comenzando desde el primer elemto cuyo indice es 0
+
+#lista.pop() ---> Aqui podemos eliminar el ultimo dato
+
+#lista.remove(2) # ---> Aqui podemos especificar algun valor que queremos eliminar
+
+lista.reverse() #Esto nos permite volcar todo alreves (Reverso)  #NOTA IMPORTANTE!!!
+# NO PODEMOS ORDENAR DATOS DE DIFERENTES TIPOS DEBEN SER SIEMPRE DEL MISMO TIPO CUANDO USAMOS O METODO DE sort
+print(lista)
+
+lista.sort()
+print(lista)
+
