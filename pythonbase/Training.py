facaade335fc5a252a92f0ec4f707cba10ba7229
EnmAsusW <enmacastle@outlook.es>
2022-11-20 22:51:54 -0300

    Incio de los ejercios

--- pythonbase/Training.py
+++ pythonbase/Training.py
@@ -1,2 +1,8 @@
 #Es hora de divertirnos rsrs
+dato = input('Ingrese un dato: ')
 
+Lista = ['Hola', 'Mudo' 'chanchito', 'feliz']
+if Lista.count(dato) > 0:
+    print("El valor se encuentra dentro de nuestros registros o listas", dato)
+    
+else: print("El valor ingresado no estaba en nuestra lista")
